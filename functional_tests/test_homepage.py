b1076e5c718c23f22e8a41f753afc8d54e8a98a2
Kyan Zhang <gn01842919@gmail.com>
2018-01-14 17:44:07 +0800

    Add FT for hompage. Failed as expected, but the browser does not close for unknown reason

---
+++ functional_tests/test_homepage.py
@@ -0,0 +1,33 @@
+from .base import FunctionalTest
+
+
+class HomepageTest(FunctionalTest):
+
+    def test_can_view_the_news_on_homepage(self):
+
+        # Go to homepage
+        self.browser.get(self.live_server_url)
+
+        # Found the url is redirected to /news/
+        expected_url = self.live_server_url + 'new/'
+        self.assertEqual(self.browser.current_url, expected_url)
+
+        # See the browser title and the header
+        self.assertIn('My Focus News!', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('My Focus News', header_text)
+
+        # There is a table containing news titles, links, and date
+        news_table = self.wait_for(lambda: self.browser.find_element_by_id('id_news_table'))
+
+        for row in news_table.find_elements_by_tag_name('tr'):
+            title = row.find_element_by_css_selector('.news_title')
+            link = row.find_element_by_css_selector('.news_link')
+            date = row.find_element_by_css_selector('.news_date')
+            print('News: {} {} {}'.format(title, link, date))
+            self.assertIsNotNone(title)
+            self.assertIsNotNone(link)
+            self.assertIsNotNone(date)
+
+        # Done
