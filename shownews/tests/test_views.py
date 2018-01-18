6024cd08818e61cc40a5ebc88eea5cf5a4d97e50
Kyan Zhang <gn01842919@gmail.com>
2018-01-18 19:21:27 +0800

    Unit test for views.py for displaying news data

--- shownews/tests/test_views.py
+++ shownews/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from shownews.models import NewsData
 
 
 class HomepageAndNewsTest(TestCase):
@@ -10,3 +11,12 @@ class HomepageAndNewsTest(TestCase):
     def test_uses_news_template(self):
         response = self.client.get('/news/')
         self.assertTemplateUsed(response, 'news.html')
+
+    def test_displays_news_data_in_the_news_page(self):
+        NewsData.objects.create(title='Title 1', url='http://url1.com')
+        NewsData.objects.create(title='Title 2', url='http://url2.com')
+
+        response = self.client.get('/news/')
+
+        self.assertContains(response, 'Title 1')
+        self.assertContains(response, 'Title 2')
