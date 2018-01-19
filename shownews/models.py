4d1b05bc1ed8800b8a0f4626a85c485cdbc0ac57
Kyan Zhang <gn01842919@gmail.com>
2018-01-19 20:26:41 +0800

    Add blueprint for model ScrapingRule

--- shownews/models.py
+++ shownews/models.py
@@ -8,3 +8,22 @@ class NewsData(models.Model):
     time = models.DateTimeField(default=timezone.now)
     creation_time = models.DateTimeField(auto_now_add=True)
     last_modified_time = models.DateTimeField(auto_now=True)
+
+
+class ScrapingRule(models.Model):
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        pass
+
+
+class NewsKeyword(models.Model):
+    rule_id = models.ManyToManyField(ScrapingRule)
+    name = models.CharField(max_length=100)
+
+    # True to include this keyword
+    # False to exclude this keyword
+    to_include = models.BooleanField()
+
+    class Meta:
+        unique_together = ('name', 'to_include')
