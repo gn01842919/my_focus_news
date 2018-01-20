c5f6ad9f754642f80bb84862b9ec6467f52c8551
Kyan Zhang <gn01842919@gmail.com>
2018-01-20 19:04:38 +0800

    Add another rule in UT of model ScrapingRule

--- shownews/tests/models/test_scraping_rule.py
+++ shownews/tests/models/test_scraping_rule.py
@@ -5,35 +5,46 @@ from shownews.models import ScrapingRule, NewsKeyword
 class ScrapingRuleBasicTest(TestCase):
 
     def test_can_save_and_retrive(self):
-        rule = ScrapingRule.objects.create()
+        rule1 = ScrapingRule.objects.create()
+        rule2 = ScrapingRule.objects.create()
         keyword1 = NewsKeyword.objects.create(name='keyword1')
         keyword2 = NewsKeyword.objects.create(name='keyword2', to_include=False)
         keyword3 = NewsKeyword.objects.create(name='keyword3')
-        rule.keywords.add(keyword1, keyword2)
-        rule.keywords.add(keyword2)
-        rule.keywords.add(keyword2)
-        rule.keywords.add(keyword3)
+        rule1.keywords.add(keyword1, keyword2)
+        rule1.keywords.add(keyword2)
+        rule1.keywords.add(keyword2)
+        rule1.keywords.add(keyword3)
 
-        saved_rule = ScrapingRule.objects.all()[0]
+        # another rule
+        rule2.keywords.add(keyword2, keyword3)
+
+        saved_rules = ScrapingRule.objects.all()
+
+        self.assertEqual(rule1, saved_rules[0])
 
         # same keyword objects will appear only once
-        self.assertEqual(saved_rule.keywords.count(), 3)
+        self.assertEqual(saved_rules[0].keywords.count(), 3)
 
-        # Can retrieve the first one
-        self.assertEqual(saved_rule.keywords.all()[0], keyword1)
+        # Can retrieve the first keyword
+        self.assertEqual(saved_rules[0].keywords.all()[0], keyword1)
 
-        # Can retrieve the second one
-        self.assertEqual(saved_rule.keywords.all()[1], keyword2)
+        # Can retrieve the second keyword
+        self.assertEqual(saved_rules[0].keywords.all()[1], keyword2)
 
         # The rule is active by default
-        self.assertTrue(saved_rule.active)
+        self.assertTrue(saved_rules[0].active)
 
         # The whole rule is correct
-        self.assertEqual(saved_rule.__str__(), "[Active] Include (keyword1, keyword3), Exclude (keyword2)")
+        self.assertEqual(saved_rules[0].__str__(), "[Active] Include (keyword1, keyword3), Exclude (keyword2)")
 
-        saved_rule.active = False
-        saved_rule.save()
-        saved_rule.full_clean()
-        saved_rule = ScrapingRule.objects.all()[0]
+        # Check that active can be set to False
+        rule1.active = False
+        rule1.full_clean()
+        rule1.save()
 
-        self.assertEqual(saved_rule.__str__(), "[Inactive] Include (keyword1, keyword3), Exclude (keyword2)")
+        saved_rules = ScrapingRule.objects.all()
+        self.assertEqual(saved_rules[0].keywords.count(), 3)
+        self.assertFalse(saved_rules[0].active)
+
+        self.assertEqual(saved_rules[0].__str__(), "[Inactive] Include (keyword1, keyword3), Exclude (keyword2)")
+        self.assertEqual(saved_rules[1].__str__(), "[Active] Include (keyword3), Exclude (keyword2)")
