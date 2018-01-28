6302feb8a9cdcc12708e173bf3f1229ec342d2ad
Kyan Zhang <gn01842919@gmail.com>
2018-01-28 18:34:01 +0800

    Fix FT

--- functional_tests/test_news_page.py
+++ functional_tests/test_news_page.py
@@ -7,8 +7,10 @@ class NewsPageTest(FunctionalTest):
     def test_can_view_all_the_news(self):
 
         # Create data for testing
-        NewsData.objects.create(title='Title 1', url='http://url1.com')
-        NewsData.objects.create(title='Title 2', url='http://url2.com')
+        news_data = [
+            NewsData.objects.create(title='Title 1', url='http://url1.com'),
+            NewsData.objects.create(title='Title 2', url='http://url2.com'),
+        ]
 
         # Go to /news/all/
         self.browser.get(self.live_server_url + '/news/all/')
@@ -21,25 +23,30 @@ class NewsPageTest(FunctionalTest):
 
         # There is a table containing news titles, links, and date
         news_table = self.wait_for(lambda: self.browser.find_element_by_id('id_news_table'))
-
         rows = news_table.find_elements_by_tag_name('tr')
-        self.assertTrue(rows)
 
-        for index, row in enumerate(rows, 1):
-            title = row.find_element_by_css_selector('.news_title')
-            url = row.find_element_by_css_selector('.news_url')
-            date = row.find_element_by_css_selector('.news_date')
-            self.assertEqual(title.text, 'Title %d' % (index))
-            self.assertEqual(url.text, 'http://url%d.com' % (index))
-            self.assertTrue(date.text)
+        self.assertEqual(len(rows), 2)
+
+        expected_titles = [news.title for news in news_data]
+        expected_urls = [news.url for news in news_data]
+
+        for index, row in enumerate(rows):
+            title = row.find_element_by_css_selector('.news_title').text
+            url = row.find_element_by_css_selector('.news_url').text
+            date = row.find_element_by_css_selector('.news_date').text
+            self.assertIn(title, expected_titles)
+            self.assertIn(url, expected_urls)
+            self.assertTrue(date)
 
         # Done
 
     def test_only_display_unread_news_by_default(self):
 
         # There are already some news...
-        NewsData.objects.create(title='Title 1', url='http://url1.com')
-        NewsData.objects.create(title='Title 2', url='http://url2.com')
+        news_data = [
+            NewsData.objects.create(title='Title 1', url='http://url1.com'),
+            NewsData.objects.create(title='Title 2', url='http://url2.com'),
+        ]
 
         # Go to homepage
         self.browser.get(self.live_server_url)
@@ -60,19 +67,25 @@ class NewsPageTest(FunctionalTest):
         )
 
         rows = news_table.find_elements_by_tag_name('tr')
+
         self.assertEqual(len(rows), 2)
 
-        for index, row in enumerate(rows, 1):
-            title = row.find_element_by_css_selector('.news_title')
-            url = row.find_element_by_css_selector('.news_url')
-            date = row.find_element_by_css_selector('.news_date')
-            self.assertEqual(title.text, 'Title %d' % (index))
-            self.assertEqual(url.text, 'http://url%d.com' % (index))
-            self.assertTrue(date.text)
+        expected_titles = [news.title for news in news_data]
+        expected_urls = [news.url for news in news_data]
+
+        for row in rows:
+            title = row.find_element_by_css_selector('.news_title').text
+            url = row.find_element_by_css_selector('.news_url').text
+            date = row.find_element_by_css_selector('.news_date').text
+            self.assertIn(title, expected_titles)
+            self.assertIn(url, expected_urls)
+            self.assertTrue(date)
 
         # More news are generated
-        NewsData.objects.create(title='Title 3', url='http://url3.com')
-        NewsData.objects.create(title='Title 4', url='http://url4.com')
+        more_news_data = [
+            NewsData.objects.create(title='Title 3', url='http://url3.com'),
+            NewsData.objects.create(title='Title 4', url='http://url4.com'),
+        ]
 
         # refresh the page
         self.browser.refresh()
@@ -85,12 +98,17 @@ class NewsPageTest(FunctionalTest):
         rows = news_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 2)
 
-        for index, row in enumerate(rows, 3):
-            title = row.find_element_by_css_selector('.news_title')
-            url = row.find_element_by_css_selector('.news_url')
-            date = row.find_element_by_css_selector('.news_date')
-            self.assertEqual(title.text, 'Title %d' % (index))
-            self.assertEqual(url.text, 'http://url%d.com' % (index))
-            self.assertTrue(date.text)
+        new_expected_titles = [news.title for news in more_news_data]
+        new_expected_urls = [news.url for news in more_news_data]
+
+        for row in rows:
+            title = row.find_element_by_css_selector('.news_title').text
+            url = row.find_element_by_css_selector('.news_url').text
+            date = row.find_element_by_css_selector('.news_date').text
+            self.assertIn(title, new_expected_titles)
+            self.assertNotIn(title, expected_titles)
+            self.assertIn(url, new_expected_urls)
+            self.assertNotIn(url, expected_urls)
+            self.assertTrue(date)
 
         # Done
