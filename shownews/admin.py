bee9f7e29798388df672c743861b068a8d9167b9
gn01842919 <gn01842919@gmail.com>
2018-03-11 21:22:34 +0800

    Remove admin.py

--- shownews/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import NewsData, ScrapingRule, NewsKeyword, NewsCategory
-
-admin.site.register(NewsData)
-admin.site.register(ScrapingRule)
-admin.site.register(NewsKeyword)
-admin.site.register(NewsCategory)
